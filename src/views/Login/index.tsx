10ae4b717f0c2d69fc3521f80119bb28ae2eacdc
chengruiHu <247071196@qq.com>
2022-11-27 20:08:47 +0800

    登录页  静态网页调整

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react'
+import { Input, Space, Button } from 'antd'
 import styles from './index.module.scss'
 import initLoginBg from './init.ts'
-import { Input, Space, Button } from 'antd'
+import "./login.less"
 
 
 
@@ -15,7 +16,7 @@ export default function Login() {
       {/* 存放背景 */}
       <canvas id="canvas" style={{ display: "block" }}></canvas>
       {/* 登录盒子 */}
-      <div className={styles.loginBox}>
+      <div className={styles.loginBox + " loginbox"}>
         {/* 标题部分 */}
         <div className={styles.title}>
           <h1>React+Vite+Ts&nbsp;.&nbsp;星空后台</h1>
@@ -24,8 +25,14 @@ export default function Login() {
         {/* 表单部分 */}
         <div className='from'>
           <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-            <Input placeholder="用户名" />
+            <Input placeholder="用户" />
             <Input.Password placeholder="密码" />
+            <div className="captchaBox">
+              <Input placeholder="验证码" />
+              <div className="captchaImg">
+                <img height="32" src="https://1234ittv.com/apis/user/api/user/getVerifyCode?uuid=98c4b47895c74a388bd9dc8e77d3d001" alt="" />
+              </div>
+            </div>
             <Button type="primary" block>
               登录
             </Button>
