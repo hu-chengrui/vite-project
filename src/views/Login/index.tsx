f5defc0a0db2854e1c2c283449ac7443fec90496
chengruiHu <247071196@qq.com>
2022-11-29 14:28:53 +0800

    登录页页面完善

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { Input, Space, Button } from 'antd'
 import styles from './index.module.scss'
 import initLoginBg from './init.ts'
@@ -11,6 +11,36 @@ export default function Login() {
     initLoginBg();
     window.onresize = function () { initLoginBg() }
   }, [])
+
+  // 修改usernameVal这个变量为用户输入的那个值。 以后拿到usernameVal这个变量就相当于拿到用户输入的信息。
+  const [usernameVal, setUsernameVal] = useState("")//定义用户输入信息这个变量
+  const [passwordVal, setPasswordVal] = useState("")//定义用户输入密码这个变量
+  const [captchaVal, setCaptchaVal] = useState("")//定义用户输入验证码这个变量
+  const usernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    // console.log(e);
+    // console.log(e.target.value);
+    setUsernameVal(e.target.value)
+  }
+  const passwordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    // console.log(e);
+    // console.log(e.target.value);
+    setPasswordVal(e.target.value)
+  }
+  const captchaChange = (e: ChangeEvent<HTMLInputElement>) => {
+    // console.log(e);
+    // console.log(e.target.value);
+    setCaptchaVal(e.target.value)
+  }
+  //点击登录按钮事件获取到上面3个值
+  const goLogin = () => {
+    console.log("用户输入的3个信息", usernameVal, passwordVal, captchaVal);
+    //验证是否有空值
+    //trim() 函数移除字符串两侧的空白字符或其他预定义字符。功能除去字符串开头和末尾的空格或其他字符。
+    // if (!usernameVal.trim() || !passwordVal.trim() || !captchaVal.trim()) {
+      
+    // }
+  }
+
   return (
     <div className={styles.login}>
       {/* 存放背景 */}
@@ -25,15 +55,15 @@ export default function Login() {
         {/* 表单部分 */}
         <div className='from'>
           <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-            <Input placeholder="用户" />
-            <Input.Password placeholder="密码" />
+            <Input placeholder="用户" onChange={usernameChange} />
+            <Input.Password placeholder="密码" onChange={passwordChange} />
             <div className="captchaBox">
-              <Input placeholder="验证码" />
+              <Input placeholder="验证码" onChange={captchaChange} />
               <div className="captchaImg">
-                <img height="32" src="https://1234ittv.com/apis/user/api/user/getVerifyCode?uuid=1d446189fba241b6a70a2a8cabbf5fad" alt="" />
+                <img height="32" src="" alt="" />
               </div>
             </div>
-            <Button type="primary" block>
+            <Button type="primary" block onClick={goLogin}>
               登录
             </Button>
           </Space>
